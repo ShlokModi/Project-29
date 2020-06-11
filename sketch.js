c83f9feed0b31e7d9e3e2fb0ea52d704f6ef3628
ShlokModi <shlokmodi2008@gmail.com>
2020-06-11 16:57:52 +0530

    xyz

--- sketch.js
+++ sketch.js
@@ -1,9 +1,55 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var ground1,block1,block2,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+  //createSprite(400, 200, 50, 50);
+  ground1 = new Ground(390,305,240,20);
+  //level one
+  block1 = new Box(300,275,30,40);
+  block2 = new Box(330,275,30,40);
+  block3 = new Box(360,275,30,40);
+  block4 = new Box(390,275,30,40);
+  block5 = new Box(420,275,30,40);
+  block6 = new Box(450,275,30,40);
+  block7 = new Box(480,275,30,40);
+  //level two
+  block8 = new Box(330,235,30,40);
+  block9 = new Box(360,235,30,40);
+  block10 = new Box(390,235,30,40);
+  block11 = new Box(420,235,30,40);
+  block12 = new Box(450,235,30,40);
+  //level three
+  block13 = new Box(360,195,30,40);
+  block14 = new Box(390,195,30,40);
+  block15 = new Box(420,195,30,40);
+  //top
+  block16 = new Box(390,155,30,40);
 }
 
 function draw() {
   background(255,255,255);  
+  Engine.update(engine);
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+  ground1.display();
   drawSprites();
 }
